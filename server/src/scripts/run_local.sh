f6ed0c75e39414feac93c191aa0ac6db77aee1b7
smarcsoft <sebmarc@yahoo.com>
2020-12-01 18:10:26 +0100

    docker compose for client and server

--- server/src/scripts/run_local.sh
+++ server/src/scripts/run_local.sh
@@ -1,1 +1,1 @@
-echo java -Djava.util.logging.config.file=config/logging.properties -jar target/grpcserver-1.0.0-jar-with-dependencies.jar
+java -Djava.util.logging.config.file=../config/logging.properties -jar ../target/grpcserver-1.0.0-jar-with-dependencies.jar
