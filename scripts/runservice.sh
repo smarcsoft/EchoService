d32d12ee35f27c3156cd7bf28c96099da88e8eda
smarcsoft <sebmarc@yahoo.com>
2020-12-26 09:34:46 +0000

    client and server communicate together

--- scripts/runservice.sh
+++ scripts/runservice.sh
@@ -1,3 +1,5 @@
+echo "Creating network..."
+docker network create --driver=overlay --attachable echonet
 echo "Creating and deploying the stack..."
 docker stack deploy -c echostack.yml echostack
 echo "Listing deployed services..."
