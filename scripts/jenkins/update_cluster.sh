8fcf6808e9554d5771b79e80991b362bd6ee2d54
smarcsoft <sebmarc@yahoo.com>
2020-12-29 18:58:10 +0100

    scripts to update cluster

--- scripts/jenkins/update_cluster.sh
+++ scripts/jenkins/update_cluster.sh
@@ -1,1 +1,6 @@
-docker service update --image $(docker inspect --type image --format '{{index .RepoDigests 0}}' sebmarc/echoservice:latest) servicename
+KEYS=$HOME/keys/smarcsoft.pem
+echo "Updating cluster with latest image version"
+echo "Connecting to manager..."
+SERVER_IP=$(az vm show -d --resource-group grpc --name manager --query "publicIps" -o tsv)
+echo "Connecting to Azure with server IP $SERVER_IP..."
+ssh -o StrictHostKeyChecking=no -i $KEYS smarcsoft@$SERVER_IP cd src/EchoService;git pull;scripts/jenkins/perform_update.sh 
