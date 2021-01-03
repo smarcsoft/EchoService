6ab14f0f1efa59bdfa5311ffd81a5cfbf33dc4a6
smarcsoft <sebmarc@yahoo.com>
2021-01-03 22:55:53 +0100

    kubectly installed in server image

--- docker/buildserver.sh
+++ docker/buildserver.sh
@@ -1,1 +1,4 @@
+#Temporality copy the AKS credentials to copy them on the image
+cp ~/.kube/config ../build
 docker build -f server/Dockerfile -t sebmarc/echoserver:latest ../build
+rm ../build/config
