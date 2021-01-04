7a14391bf59e67ed7ef143629fd1fff967c121f0
smarcsoft <sebmarc@yahoo.com>
2021-01-04 23:36:55 +0100

    autoscaling works (with some grpc errors)

--- scripts/deploy_service.sh
+++ scripts/deploy_service.sh
@@ -2,4 +2,5 @@ echo "Running deployment..."
 kubectl apply -f aks/echo-deployment.yaml
 echo "Creating the service"
 kubectl apply -f aks/echo-service.yaml
+kubectl apply -f aks/echo-scaler.yaml
 kubectl get services -o wide
